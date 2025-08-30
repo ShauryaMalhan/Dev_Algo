3b4449255355737b19dbdf2e1a9dcfe5887edf2c
ShauryaMalhan <seemashaurya13@gmail.com>
2025-08-30 10:02:23 +0530

    Fixed config file bug

--- oj/vite.config.js
+++ oj/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { sitemap } from 'vite-plugin-sitemap';
+import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
   plugins: [
     react(),
     sitemap({
-      // Ensure this is your correct, live domain name
       hostname: 'https://picode.polsage.in', 
     }),
   ],
