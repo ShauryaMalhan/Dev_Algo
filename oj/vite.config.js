8965a4dda41372b4afba50f078fea1eab2a445be
ShauryaMalhan <seemashaurya13@gmail.com>
2025-08-30 09:55:30 +0530

    Edited Vite.config

--- oj/vite.config.js
+++ oj/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { sitemap } from 'vite-plugin-sitemap';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    sitemap({
+      // Ensure this is your correct, live domain name
+      hostname: 'https://picode.polsage.in', 
+    }),
+  ],
+});
+
