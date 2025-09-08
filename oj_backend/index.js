23f9867dedb6b923c647ffbbcce6207538600251
ShauryaMalhan <seemashaurya13@gmail.com>
2025-09-08 12:59:47 +0530

    CI/CD

--- oj_backend/index.js
+++ oj_backend/index.js
@@ -11,9 +11,9 @@ import Profile from './routes/profile.js';
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 DBConnection();
 
@@ -21,13 +21,13 @@ app.get('/', (req, res) => {
     res.send("Backend is running");
 })
 
-app.use('/api/auth', authRoutes); // Auth Routes
-app.use('/api/problems', problemRoutes); // Problems Routes
-app.use('/api/cppCompiler', authCompiler); // Compiler Routes
-app.use('/api/submissions', History); // Submission Routes
-app.use('/api/admin', Admin); // Admin Routes
-app.use('/api/user', User); // User Routes
-app.use('/api/profile', Profile); // Profile Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/problems', problemRoutes);
+app.use('/api/cppCompiler', authCompiler);
+app.use('/api/submissions', History);
+app.use('/api/admin', Admin);
+app.use('/api/user', User);
+app.use('/api/profile', Profile);
 
 app.listen(8000, (req, res) => {
     console.log('listening on port 8000');
