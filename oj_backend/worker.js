6be6715bd967803b46f2e4d23c4d230d25bd290b
ShauryaMalhan <seemashaurya13@gmail.com>
2025-09-08 18:40:21 +0530

    Frontend Bug Fixed

--- oj_backend/worker.js
+++ oj_backend/worker.js
@@ -98,7 +98,7 @@ const worker = new Worker('submissions', judge, {
         host: 'redis_queue',
         port: 6379
     },
-    concurrency: 5
+    concurrency: 2
 });
 
 console.log("Judge worker started...");
