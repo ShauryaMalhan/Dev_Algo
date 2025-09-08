c891aefc9b03898b7a4ea54026e2e50012a6cd6b
ShauryaMalhan <seemashaurya13@gmail.com>
2025-09-08 18:07:27 +0530

    fix: Resolve Vercel build error and update submission pages

--- oj/vite.config.js
+++ oj/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://picode.polsage.in', 
+    }),
+  ],
 });
 
